7bc3d600561e74c00a784f95881d9aae29179702
ehofm <ellahofmanncoyle@gmail.com>
2021-03-14 01:17:31 -0600

    dataloader + IDF strategy + and IDF analysis pics

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import datasets
 
+"""
+Returns raw wikitext train input
+"""
 def get_data():
+    # return wikitext dataset
     dataset = datasets.load_dataset('wikitext', 'wikitext-2-v1')
     return dataset['train']['text']
